e07a7903eccfc253302be5c53492e391d2194838
Mustafa Ozbalci <mustafa.ozbalci@etiya.com>
2023-12-27 14:42:08 +0300

    Commit

---
+++ src/main/java/hotel/employee/Notification.java
@@ -0,0 +1,23 @@
+package hotel.employee;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "notification", schema = "hotel")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notification_id")
+    private Long notificationId;
+
+    private String message;
+    private String status; // "ACTIVE" veya "PASSIVE" olabilir
+    private int roomNumber;
+}
